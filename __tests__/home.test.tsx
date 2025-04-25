b2cec57adddccbbc7bea1b1448159aad13ed91ad
ake <tamiratgedi1@gmail.com>
2025-04-25 17:35:17 +0300

    jest test

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -1,19 +1,53 @@
+import Home from '@/app/[lang]/admin/page';
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 
-import Loading from '@/app/[lang]/(main)/loading/page'
 
 it('Loading component should render correctly', () => {
-render(<Loading />)
+    render(<Home  />)
 
-const heading = screen.getByRole('heading', { level: 3 })
-const headingText = screen.getByText(/This is the admin dashboard/i)
-
-expect(heading).toBeInTheDocument()
-expect(headingText).toBeInTheDocument()
-expect(heading).toHaveTextContent('This is the admin dashboard')
-expect(heading).toHaveClass('text-2xl text-black font-semibold')
-
+    const heading = screen.getByRole('heading', { name: /This is the admin dashboard/i, hidden:true }, );
+    expect(heading).toBeInTheDocument();
 })
 
 
+// jest.mock('../components/admin/main', () => {
+//     return   jest.fn(()=><div>StatCard Mock</div>)
+// }
+// )
+
+// it('renders StatCard component', ()=>{
+//     render(<Home  />)
+//     expect(screen.getByText('StatCard Mock')).toBeInTheDocument()
+// })
+
+
+// it('has correct class names for the heading',()=>{
+//     render(<Home  />)
+//     const heading = screen.getByRole('heading',{ name: /This is the admin dashboard/i, hidden:true }, );
+//     expect(heading).toHaveClass('text-2xl font-bold text-gray-900 dark:text-white')
+//     expect(heading).toHaveClass('mb-4')
+//     expect(heading).toHaveClass('font-bold')
+// })
+
+
+it('matches the snapshot', ()=>{
+    const {asFragment}  = render(<Home  />)
+    expect(asFragment()).toMatchSnapshot()
+}
+)
+
+
+// it('Loading component should render correctly', () => {
+// render(<Loading />)
+// const headingText = screen.getByText(/This is the admin dashboard/i)
+
+// expect(headingText).toBeInTheDocument()
+
+// })
+
+
+
+// test('two plus two is four', () => {
+//     expect(2 + 2).toBe(4);
+//   });
--- app/[lang]/admin/page.tsx
+++ app/[lang]/admin/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
       <>
           <div className='flex flex-col md:flex-row justify-between gap-5 mb-5 text-white'>
-              <h3 className='text-2xl text-black font-semibold'>This is the admin dashboard</h3>
+              <h1 className='text-2xl text-black font-semibold'>This is the admin dashboard</h1>
           </div>
           <div className='p-4 rounded-lg shadow-lg px-6 flex-1'>
               <StatCard />
